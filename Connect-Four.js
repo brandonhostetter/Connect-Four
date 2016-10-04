86f6b9488b88681deda10a4960a831def432889b
Brandon Hostetter <bbh@email.sc.edu>
2016-10-03 20:20:20 -0400

    Added ability to connect to another player
    Using firebase, the moves are updated in real-time so two players, on two separate computers, can connect and play a game together

--- Connect-Four.js
+++ Connect-Four.js
@@ -10,38 +10,109 @@ Page.canvasWidth = 545;
 Page.canvasHeight = 470;
 Page.rows = 6;
 Page.columns = 7;
+// dom selectors
+Page.$initModal = null;
+Page.$connectPlayersModal = null;
+Page.$gameOverModal = null;
 // player related variables
 Page.playerTurn = null;
+Page.playerNumber = -1;
 Page.piececolors = ['gold', 'orangered'];
 Page.placementArray = null;
 Page.placementOrder = null;
 Page.y = 0;
 Page.dy = 20;
-Page.drawPieceAt = null;
+Page.piecePlacedAt = null;
 Page.isGameOver = false;
-// options variables
-Page.enableAnimations = true;
+Page.gameUUID = '';
+Page.gameStarted = false;
 
-Page.initialize = function () {
+Page.initialize = function() {
     Page.$canvas = $('#connect-four-canvas');
     Page.context = Page.$canvas[0].getContext('2d');
     Page.$maskCanvas = $('#connect-four-mask-canvas');
     Page.maskContext = Page.$maskCanvas[0].getContext('2d');
 
-    Page.placementArray = Page.create2DArray(Page.rows, Page.columns);
+    Page.$initModal = $('#init-modal');
+    Page.$connectPlayersModal = $('#connect-players-modal');
+    Page.$gameOverModal = $('#game-over-modal');
+
+    Page.placementArray = Helpers.create2DArray(Page.rows, Page.columns);
     Page.placementOrder = [];
+    Page.piecePlacedAt = [];
+
+    Page.drawOpeningMask();
+
+    Page.determineOpeningAction();
+};
+
+//#region Initialization
+
+Page.determineOpeningAction = function() {
+    Page.gameUUID = Helpers.readUrlHash();
+
+    if (Page.gameUUID != '') {
+        // Join existing game
+        // this is player 2
+        Page.playerNumber = 1;
+        Page.joinExistingGame();
+    } else {
+        // No existing game to join
+        // Instead, ask the user if they would like to play on one computer or two
+        // this is player 1
+        Page.playerNumber = 0;
+        Page.displayInitModal();
+    }
+};
+
+Page.displayInitModal = function() {
+    Page.$initModal.modal('show');
+
+    // play on one computer
+    Page.$initModal.find('#play-one-computer').on('click', function() {
+        Page.$initModal.modal('hide');
+    });
+
+    // play on two computers (connect two games)
+    Page.$initModal.find('#play-two-computers').on('click', function() {
+        Helpers.showLoadingOverlay(); // display an overlay until player 2 joins
+        Page.multiComputer();
+        Page.createGameData();
+        Page.getGameData();
+        Page.$initModal.modal('hide');
+    });
+};
+
+Page.attachListeners = function() {
     Page.attachCanvasListener();
     Page.attachButtonListeners();
+};
 
-    Page.playerTurn = 0;
-    Page.drawOpeningMask();
+Page.multiComputer = function() {
+    // Generate a unique id for the game
+    Page.gameUUID = Helpers.generateGUID();
+    // Change the url to reflect the unique id
+    window.location = '#' + Page.gameUUID;
+    // Update the modal with the unique game url
+    Page.$connectPlayersModal.find('#my-uuid').val(window.location.href);
+    Page.$connectPlayersModal.modal('show');
+
+    Page.$connectPlayersModal.find('#copy-uuid-btn').on('click', function(e) {
+        var target = Page.$connectPlayersModal.find('#my-uuid')[0];
+        Helpers.copyToClipboard(target);
+    });
+};
+
+Page.joinExistingGame = function() {
+    Page.getGameData();
+    Page.player2Ready();
 };
 
 //#region Event Listeners
 
-Page.attachCanvasListener = function () {
-    Page.$maskCanvas.on('mouseup', function (e) {
-        if (Page.isAnimating) {
+Page.attachCanvasListener = function() {
+    Page.$maskCanvas.on('mouseup', function(e) {
+        if (Page.isAnimating || (Page.playerTurn != Page.playerNumber)) {
             return;
         } //else
 
@@ -52,38 +123,39 @@ Page.attachCanvasListener = function () {
         if (col < 0) col = 0;
         if (col >= Page.columns) col = Page.columns - 1;
 
-        Page.drawPieceAt = Page.forcePieceToBottom(col);
-        if (Page.drawPieceAt) {
+        Page.piecePlacedAt = Helpers.forcePieceToBottom(Page.placementArray, col);
+        if (Page.piecePlacedAt) {
             Page.draw();
         }
     });
 };
 
-Page.attachButtonListeners = function () {
-    $('#undo-button').on('click', function (e) {
+Page.attachButtonListeners = function() {
+    $('#undo-button').on('click', function(e) {
         if (Page.isGameOver) return;
 
         var length = Page.placementOrder.length;
         if (length > 0) {
             var last = Page.placementOrder[length - 1];
-            Page.placementArray[last.row][last.col].player = null;
-            Page.placementArray[last.row][last.col].color = 'white';
-            Page.playerTurn = (Page.playerTurn + 1) % 2;
+            Page.placementArray[last.row][last.col].player = -1;
+            Helpers.updatePlayerTurn();
             Page.drawUndo();
             Page.placementOrder.pop();
         }
     });
 
-    $('#restart-button').on('click', function (e) {
+    $('#restart-button').on('click', function(e) {
         Page.resetGame();
     });
 };
 
 //#endregion Event Listeners
 
+//#endregion Initialization
+
 //#region Canvas Drawing
 
-Page.draw = function () {
+Page.draw = function() {
     Page.intervalId = window.requestAnimationFrame(Page.draw);
     Page.context.clearRect(0, 0, Page.canvasWidth, Page.canvasHeight);
 
@@ -91,7 +163,15 @@ Page.draw = function () {
     Page.drawDropAnimation();
 };
 
-Page.drawOpeningMask = function () {
+Page.drawOpponentMove = function() {
+    Page.intervalId = window.requestAnimationFrame(Page.drawOpponentMove);
+    Page.context.clearRect(0, 0, Page.canvasWidth, Page.canvasHeight);
+
+    Page.drawPlacedPieces(true);
+    Page.drawOpponentAnimation();
+};
+
+Page.drawOpeningMask = function() {
     // http://stackoverflow.com/a/11770000
     // We draw this once at initialization. This is the 'cut-out' of the holes
     // the pieces will fill;
@@ -105,7 +185,7 @@ Page.drawOpeningMask = function () {
     Page.maskContext.fill();
 };
 
-Page.drawPiece = function (x, y, player) {
+Page.drawPiece = function(x, y, player) {
     Page.context.beginPath();
     Page.context.arc(x, y, 30, 0, 2 * Math.PI);
     Page.context.fillStyle = Page.piececolors[player];
@@ -113,97 +193,153 @@ Page.drawPiece = function (x, y, player) {
     Page.context.closePath();
 };
 
-Page.drawDropAnimation = function () {
-    if (Page.stopRendering || !Page.enableAnimations) {
+Page.drawDropAnimation = function() {
+    if (Page.stopRendering) {
         cancelAnimationFrame(Page.intervalId);
         Page.y = 0;
         Page.stopRendering = false;
-        Page.updatePlacement();
+        // update placement array
+        Helpers.updatePlacement(Page.placementArray, Page.placementOrder, Page.piecePlacedAt, Page.playerTurn);
+
+        // check for winner
+        if (Page.isThereAWinner()) {
+            Page.gameOver();
+        }
+
+        // update database
+        Page.saveGameData();
+
         Page.drawPlacedPieces();
         Page.isAnimating = false;
         return;
-    } else if (Page.y >= Page.drawPieceAt[0] * 96 - 62) {
+    } else if (Page.y >= Page.piecePlacedAt[0] * 96 - 62) {
         Page.stopRendering = true;
         return;
     }
 
     Page.isAnimating = true;
     Page.y += Page.dy;
-    Page.drawPiece(75 * Page.drawPieceAt[1] + 50, Page.y, Page.playerTurn);
+    Page.drawPiece(75 * Page.piecePlacedAt[1] + 50, Page.y, Page.playerTurn);
 };
 
-Page.drawPlacedPieces = function () {
+Page.drawOpponentAnimation = function() {
+    if (Page.stopRendering) {
+        cancelAnimationFrame(Page.intervalId);
+        Page.y = 0;
+        Page.stopRendering = false;
+        Page.drawPlacedPieces();
+        Page.isAnimating = false;
+        return;
+    } else if (Page.y >= Page.piecePlacedAt[0] * 96 - 62) {
+        Page.stopRendering = true;
+        return;
+    }
+
+    Page.isAnimating = true;
+    Page.y += Page.dy;
+    // we should be on the current player's turn so we need to change player turn back to the opponent
+    Page.drawPiece(75 * Page.piecePlacedAt[1] + 50, Page.y, ((Page.playerTurn + 1) % 2));
+};
+
+Page.drawPlacedPieces = function(ignoreMostRecentlyPlaced) {
     for (var i = 0; i < Page.placementArray.length; i++) {
         for (var j = 0; j < Page.placementArray[i].length; j++) {
-            if (Page.placementArray[i][j].player !== null) {
+            if (Page.placementArray[i][j].player !== -1) {
+                if (ignoreMostRecentlyPlaced && Page.piecePlacedAt[0] === i && Page.piecePlacedAt[1] === j) {
+                    continue;
+                }
                 Page.drawPiece(75 * j + 50, 75 * i + 48, Page.placementArray[i][j].player);
             }
         }
     }
 };
 
-Page.drawUndo = function () {
+Page.drawUndo = function() {
     Page.context.clearRect(0, 0, Page.canvasWidth, Page.canvasHeight);
     Page.drawPlacedPieces();
 };
 
 //#endregion Canvas Drawing
 
-//#region Helpers
+//#region Update game data
 
-Page.create2DArray = function (row, col) {
-    var arr = [];
-    for (var i = 0; i < row; i++) {
-        arr.push([]);
-        for (var j = 0; j < col; j++) {
-            arr[i].push({ player: null, color: 'white' });
-        }
-    }
-    return arr;
+Page.createGameData = function() {
+    // create the empty placement array on the database
+    database.ref('connect-four/' + Page.gameUUID).set({
+        'placementArray': Page.placementArray,
+        'player2Ready': false // player 1 creates the game so assume they're ready
+    });
+
+    database.ref('connect-four/' + Page.gameUUID + '/turn').set({
+        'playerTurn': 0,
+        'gameOver': false,
+        'drawPieceAt': [-1, -1]
+    });
 };
 
-Page.isLocationTaken = function (row, column) {
-    if (Page.placementArray[row][column].player !== null) {
-        return true;
-    }
-    return false;
+Page.player2Ready = function() {
+    database.ref('connect-four/' + Page.gameUUID).update({
+        'player2Ready': true
+    });
 };
 
-Page.forcePieceToBottom = function (column) {
-    // drop the piece in the column the user clicked,
-    // not necessarily the exact location that was clicked
-    var validMove = null;
-    var row = Page.rows - 1;
+Page.saveGameData = function() {
+    var updates = {};
+    updates['connect-four/' + Page.gameUUID + '/placementArray/' + Page.piecePlacedAt[0] + '/' + Page.piecePlacedAt[1]] = {
+        'player': Page.playerTurn
+    };
+    updates['connect-four/' + Page.gameUUID + '/turn'] = {
+        'playerTurn': Helpers.updatePlayerTurn(),
+        'drawPieceAt': Page.piecePlacedAt,
+        'gameOver': Page.isGameOver
+    };
 
-    while (!validMove) {
-        if (Page.isLocationTaken(row, column)) {
-            row -= 1;
-            if (row < 0) {
-                break;
+    Page.playerTurn = Helpers.updatePlayerTurn();
+    return database.ref().update(updates);
+};
+
+Page.getGameData = function() {
+    database.ref('connect-four/' + Page.gameUUID).on('value', function(snapshot) {
+        if (snapshot.val()) {
+            Page.placementArray = snapshot.val().placementArray;
+
+            // if (Page.playerTurn !== snapshot.val().turn.playerTurn) {
+            Page.playerTurn = snapshot.val().turn.playerTurn;
+            console.log(Page.playerTurn);
+            Helpers.updateTurnLabels(Page.playerTurn, Page.playerNumber);
+            // }
+
+            if (snapshot.val().turn.drawPieceAt[0] !== -1 &&
+                snapshot.val().turn.drawPieceAt[1] !== -1) {
+                if (Page.piecePlacedAt[0] !== snapshot.val().turn.drawPieceAt[0] ||
+                    Page.piecePlacedAt[1] !== snapshot.val().turn.drawPieceAt[1]) {
+                    Page.piecePlacedAt = snapshot.val().turn.drawPieceAt;
+                    Page.drawOpponentMove();
+                }
+            }
+
+            if (snapshot.val().turn.gameOver && !Page.isGameOver) {
+                Page.gameOver();
+            }
+
+            if (!Page.gameStarted && snapshot.val().player2Ready) {
+                Page.attachListeners();
+                Helpers.hideLoadingOverlay();
+                Page.$connectPlayersModal.modal('hide');
+                Page.gameStarted = true;
             }
         } else {
-            validMove = [row, column];
+            console.warn('error getting data')
         }
-    }
-    return validMove;
+        console.log(snapshot.val());
+    });
 };
 
-Page.updatePlacement = function () {
-    Page.placementArray[Page.drawPieceAt[0]][Page.drawPieceAt[1]].player = Page.playerTurn;
-    Page.placementOrder.push({ row: Page.drawPieceAt[0], col: Page.drawPieceAt[1] });
-
-    if (Page.isThereAWinner()) {
-        Page.gameOver();
-    }
-
-    Page.playerTurn = (Page.playerTurn + 1) % 2;
-};
-
-//#endregion Helpers
+//#endregion Update game data
 
 //#region Win Condition
 
-Page.isThereAWinner = function () {
+Page.isThereAWinner = function() {
     var p = Page.playerTurn;
     var matchesHorizontal = 0;
     var matchVertical = 0;
@@ -244,20 +380,20 @@ Page.isThereAWinner = function () {
     return false;
 };
 
-Page.checkWinDiagonal = function (i, j) {
+Page.checkWinDiagonal = function(i, j) {
     var p = Page.playerTurn;
     if (Page.placementArray[i][j].player === p && (i + 3 < Page.rows)) {
         if (j + 3 < Page.columns) {
-            if (Page.placementArray[i + 1][j + 1].player === p
-				&& Page.placementArray[i + 2][j + 2].player === p
-				&& Page.placementArray[i + 3][j + 3].player === p) {
+            if (Page.placementArray[i + 1][j + 1].player === p &&
+                Page.placementArray[i + 2][j + 2].player === p &&
+                Page.placementArray[i + 3][j + 3].player === p) {
                 return true;
             }
         }
         if (j - 3 > -1) {
-            if (Page.placementArray[i + 1][j - 1].player === p
-				&& Page.placementArray[i + 2][j - 2].player === p
-				&& Page.placementArray[i + 3][j - 3].player === p) {
+            if (Page.placementArray[i + 1][j - 1].player === p &&
+                Page.placementArray[i + 2][j - 2].player === p &&
+                Page.placementArray[i + 3][j - 3].player === p) {
                 return true;
             }
         }
@@ -268,53 +404,26 @@ Page.checkWinDiagonal = function (i, j) {
 
 //#endregion Win Condition
 
-//#region Options
-
-Page.animationOption = function () {
-    Page.enableAnimations = !Page.enableAnimations;
-};
-
-Page.restartOption = function () {
-    $('#game-over-modal').on('show.bs.modal', function (e) {
-        $(this).find('#confirm-replay-game').on('click', function () {
-            Page.resetGame();
-            $('#game-over-modal').modal('hide');
-        });
-    });
-
-    $('#game-over-modal').modal('show');
-};
-
-Page.openOptions = function () {
-    $('#options-side-menu').css('width', '250px');
-};
-
-Page.closeOptions = function () {
-    $('#options-side-menu').css('width', '0');
-};
-
-//#endregion Options
-
 //#region Reset / Replay
 
-Page.gameOver = function () {
+Page.gameOver = function() {
     Page.$maskCanvas.off('mouseup');
     Page.isGameOver = true;
 
-    $('#game-over-modal').on('show.bs.modal', function (e) {
-        $(this).find('#confirm-replay-game').on('click', function () {
+    Page.$gameOverModal.on('show.bs.modal', function(e) {
+        $(this).find('#confirm-replay-game').on('click', function() {
             Page.resetGame();
-            $('#game-over-modal').modal('hide');
+            Page.$gameOverModal.modal('hide');
         });
     });
 
-    $('#game-over-modal').modal('show');
+    Page.$gameOverModal.modal('show');
 };
 
-Page.resetGame = function () {
+Page.resetGame = function() {
     Page.context.clearRect(0, 0, Page.canvasWidth, Page.canvasHeight);
     Page.playerTurn = 0;
-    Page.placementArray = Page.create2DArray(Page.rows, Page.columns);
+    Page.placementArray = Helpers.create2DArray(Page.rows, Page.columns);
     Page.placementOrder = [];
     Page.attachCanvasListener();
     Page.isGameOver = false;
@@ -322,6 +431,6 @@ Page.resetGame = function () {
 
 //#endregion Reset / Replay
 
-$(document).on('ready', function () {
+$(document).on('ready', function() {
     Page.initialize();
 });
